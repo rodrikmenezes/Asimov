5f26739bb8ff8190eeb8461274fb940870c90f82
EaD09206611 <rodrikaraujo@gmail.com>
2024-09-24 22:20:45 -0300

    proj

--- Python-Starter/Projetos/__MinhasRespostas__/organizador-de-arquivos/in.py
+++ Python-Starter/Projetos/__MinhasRespostas__/organizador-de-arquivos/in.py
@@ -7,31 +7,31 @@ diretorio = os.getcwd()
 
 # Listar tipos de arquivos dentro da pasta diretório
 lista_arquivos = []
-for x in os.listdir(diretorio):
-    if os.path.isfile(x):
-        lista_arquivos.append(x)
+for arquivo in os.listdir(diretorio):
+    if os.path.isfile(arquivo):
+        lista_arquivos.append(arquivo)
 
-# Eliminar py
-# lista_extensoes.remove('py')
-
+# Eliminar in.py
+lista_arquivos_sem_py = [x for x in lista_arquivos if not x.endswith('.py')]
 
 # Obter extensões 
 extensoes = []
-for x in lista_arquivos:
-    y = x.split(sep='.')[1]
+for x in lista_arquivos_sem_py:
+    y = x.split('.')[1]
     extensoes.append(y)
 
 # Eliminar duplicados
-lista_extensoes = list(set(extensoes))
-
+extensoes_sem_duplicados = list(set(extensoes))
 
 # Criar pastas com o nome das extensões
-for x in lista_extensoes:
-    os.mkdir(x)
+for x in extensoes_sem_duplicados: os.mkdir(x)
 
 # Mover arquivos
-for x in lista_arquivos: print(x)
-
+for x in lista_arquivos_sem_py:
+    extensao = x.split('.')[1]
+    origem = os.path.join(diretorio, x)
+    destino = os.path.join(diretorio, extensao, x)
+    os.rename(origem, destino)
 
 
 
